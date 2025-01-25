c901b2f6c071f052c0faa58df01cccb6d44fe5f4
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2025-01-25 10:33:46 -0300

    feat: Creating mobile sidebar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -3,10 +3,12 @@ import { Button } from "@/components/ui/button";
 import AppRoutes from "@/util/appRoutes";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
+import { MobileSidebar } from "./mobile-navbar";
 
 export const Navbar = () => {
   return (
     <div className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
--- config/site.ts
+++ config/site.ts
@@ -2,4 +2,5 @@ export const siteConfig = {
   name: "PI Planning Assistant",
   description: "Integrate multiple boards!",
   defaultStorageKey: "t-sidebar-state",
+  defaultMobileStorageKey: "t-sidebar-mobile-state",
 };
