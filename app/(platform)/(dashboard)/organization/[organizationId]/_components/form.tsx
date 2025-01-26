16b3900a23594b6cb17296d7621abf4a5eda5086
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2025-01-26 11:24:53 -0300

    feat: Created form submit button component

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/form.tsx
@@ -2,7 +2,7 @@
 
 import { createBoard } from "@/actions/create-board";
 import { FormInput } from "@/components/form/form-input";
-import { Button } from "@/components/ui/button";
+import { FormSubmit } from "@/components/form/form-submit";
 import { useAction } from "@/hooks/use-action";
 
 export const Form = () => {
@@ -29,7 +29,7 @@ export const Form = () => {
       <div className="flex flex-col space-y-2">
         <FormInput id="title" label="Board Title" errors={fieldErrors} />
       </div>
-      <Button type="submit">Submit</Button>
+      <FormSubmit>Save</FormSubmit>
     </form>
   );
 };
