3aaa9693805c68c9e25ef5b367e7cfaa5edb5488
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2025-01-26 14:23:49 -0300

    feat: Creating board list component

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,9 +1,26 @@
 import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
+import { Skeleton } from "@/components/ui/skeleton";
+import { db } from "@/lib/db";
+import AppRoutes, { buildRoute } from "@/util/appRoutes";
+import { auth } from "@clerk/nextjs/server";
 import { HelpCircle, User2 } from "lucide-react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export const BoardList = () => {
-  const maximumFreeBoards = 5;
+const maximumFreeBoards = 5;
+
+export const BoardList = async () => {
+  const { orgId } = await auth();
+
+  if (!orgId) {
+    return redirect(AppRoutes.SELECT_ORGANIZATION);
+  }
+
+  const boards = await db.board.findMany({
+    where: { orgId },
+    orderBy: { createdAt: "desc" },
+  });
 
   return (
     <div className="space-y-4">
@@ -12,6 +29,17 @@ export const BoardList = () => {
         Your boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        {boards.map((board) => (
+          <Link
+            key={board.id}
+            href={buildRoute(AppRoutes.BOARD_ID, { boardId: board.id })}
+            style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+            className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
+          >
+            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+            <p className="relative font-semibold text-white">{board.title}</p>
+          </Link>
+        ))}
         <FormPopover sideOffset={10} side="right">
           <div
             role="button"
@@ -27,6 +55,21 @@ export const BoardList = () => {
           </div>
         </FormPopover>
       </div>
+    </div>
+  );
+};
+
+BoardList.Skeleton = function SkeletonBoardList() {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <Skeleton className="aspect-video h-[70px] w-[120px]" />
+      <Skeleton className="aspect-video h-[70px] w-[120px]" />
+      <Skeleton className="aspect-video h-[70px] w-[120px]" />
+      <Skeleton className="aspect-video h-[70px] w-[120px]" />
+      <Skeleton className="aspect-video h-[70px] w-[120px]" />
+      <Skeleton className="aspect-video h-[70px] w-[120px]" />
+      <Skeleton className="aspect-video h-[70px] w-[120px]" />
+      <Skeleton className="aspect-video h-[70px] w-[120px]" />
     </div>
   );
 };
