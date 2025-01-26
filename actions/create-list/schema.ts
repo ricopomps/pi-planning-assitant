7058e2b8d5a930870bbbcec51dfb1f23c2372602
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2025-01-26 16:37:40 -0300

    Feat: Creating new lists

---
+++ actions/create-list/schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+const minimumTitleSize = 3;
+
+export const CreateList = z.object({
+  title: z
+    .string({
+      required_error: "Title is required",
+      invalid_type_error: "Title is invalid",
+    })
+    .min(minimumTitleSize, {
+      message: `Minimum length of ${minimumTitleSize} letters is required`,
+    }),
+  boardId: z.string(),
+});
--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,5 +1,41 @@
-const BoardIdPage = () => {
-  return <div>BOARD ID</div>;
+import { db } from "@/lib/db";
+import AppRoutes from "@/util/appRoutes";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
+
+interface BoardIdPageProps {
+  params: { boardId: string };
+}
+const BoardIdPage = async ({ params: { boardId } }: BoardIdPageProps) => {
+  const { orgId } = await auth();
+
+  if (!orgId) {
+    redirect(AppRoutes.SELECT_ORGANIZATION);
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: boardId,
+      board: { orgId },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={boardId} data={lists} />
+    </div>
+  );
 };
 
 export default BoardIdPage;
